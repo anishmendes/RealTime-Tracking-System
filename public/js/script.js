b40590d9ef0702401acef6bfa1f8e48f0a9b9b7e
Anish Mainali <anishmainali31@gmail.com>
2024-09-14 17:29:51 +0545

    show on view

--- public/js/script.js
+++ public/js/script.js
@@ -14,7 +14,7 @@ if(navigator.geolocation) {
 })
 }
 
-const map = L.map("map").setView([0,0], 10);
+const map = L.map("map").setView([0,0],16);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "OpenStreetMap"
@@ -24,11 +24,11 @@ const markers = {};
 
 socket.on("receive-location", (data)=> {
     const {id, latitude,longitude} = data;
-    map.setView([latitude,longitude], 16);
+    map.setView([latitude,longitude]);
     if (markers[id]) {
         markers[id].setLatLng([latitude, longitude]);
     }
     else{
-        markers[id] = L.markers([latitude, longitude]).addTo(map);
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
     }
 })
